fa767b1c228f6cd895e9eaa91d5f33b1689e7e8e
Dilka Sithari <sitharidilka@gmail.com>
2023-01-23 09:48:55 +0530

    first commit

---
+++ src/main/java/com/example/springbootuploadfilesdatabase/SpringBootUploadFilesDatabaseApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootuploadfilesdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadFilesDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUploadFilesDatabaseApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
